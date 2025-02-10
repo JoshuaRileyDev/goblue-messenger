f618f881c9e72426230b4e55e9e667f0966806ec
Joshua Riley <joshua@joshuariley.co.uk>
2025-02-10 15:26:22 +0000

    Initial Commit

---
+++ goblue/goblueApp.swift
@@ -0,0 +1,17 @@
+//
+//  goblueApp.swift
+//  goblue
+//
+//  Created by Joshua Riley on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct goblueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
